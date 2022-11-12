2501c63aa960f7f3befc13da61d6d74e55dfb931
Genteki <zhangkaiyuan.null@gmail.com>
2022-11-12 15:25:20 -0500

    Initial Commit

---
+++ Mass-Spring-Simulator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Mass-Spring-Simulator
+//
+//  Created by Genteki on 2022/11/12.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
